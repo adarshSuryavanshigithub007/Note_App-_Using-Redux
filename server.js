f6a99105108c7caea42578b2968fc1d1eab59857
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2024-07-12 23:22:38 +0530

    test

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const connectionDB = require('./config/db');
+
+const app = express()
+//.env
+dotenv.config()
+
+app.use(cors())
+
+//database connection
+connectionDB()
+
+// middleweres
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.get('/',(res,req)=>{
+    res.status(200).send({
+        message: '8080'
+    })
+})
+
+//routes
+app.use('/api/v1/user',require('./routes/userRoutes'))
+
+//Listen port
+const port = process.env.PORT || 8080
+
+app.listen(port,()=>{
+    console.log(`Server is running on dev mode ${process.env.NODE_MODE}  Server is running on porthttp://localhost: ${process.env.PORT}`.bgCyan.white)
+    
+})
